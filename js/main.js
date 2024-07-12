14b159e9d7bf8604729b1a3714eb1f13fa0147d8
Juanhernandez1424 <juanhr1424@gmail.com>
2024-07-12 12:15:37 -0500

    skills en javascript de forma dinámica

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 const header = document.querySelector('.header')
 const btnCv = document.getElementById('download-cv')
-const spans = document.querySelectorAll('.progress-bar span')
 const btnHamburgusa = document.getElementById('btn_hamburguesa')
 const navBar = document.getElementById('nav-bar')
 const links = document.querySelectorAll('#links')
-
-console.log(links)
+const skills = document.getElementById('skills')
 
 let scrollLast = 0
 window.addEventListener('scroll', () => {
@@ -22,11 +20,83 @@ btnCv.addEventListener('click', () => {
     window.location.href = 'https://drive.google.com/uc?id=1ri3QarfQPjAKKrvbVDn6ZXA-5xC8smSS&export=download'
 })
 
-spans.forEach((span) => {
-    let spanInner = span.style.width = span.dataset.width
-    span.innerHTML = spanInner
-})
-
 btnHamburgusa.addEventListener('click', () => {
     navBar.classList.toggle('active')
+})
+
+
+const habilidades = [
+    {
+        nombre: 'HTML',
+        porcentaje: '90%',
+        valor: '90',
+    },
+    {
+        nombre: 'CSS',
+        porcentaje: '70%',
+        valor: '70',
+    },
+    {
+        nombre: 'JavaScript',
+        porcentaje: '60%',
+        valor: '60',
+    },
+    {
+        nombre: 'Angular',
+        porcentaje: '30%',
+        valor: '30',
+    },
+    {
+        nombre: 'SQL Server',
+        porcentaje: '50%',
+        valor: '50',
+    },
+    {
+        nombre: 'Git - GitHub',
+        porcentaje: '50%',
+        valor: '50',
+    },
+    {
+        nombre: 'Azure Devops',
+        porcentaje: '50%',
+        valor: '50',
+    },
+    {
+        nombre: 'Historias de Usuario',
+        porcentaje: '90%',
+        valor: '90',
+    },
+    {
+        nombre: 'Pruebas Funcionales',
+        porcentaje: '100%',
+        valor: '100',
+    },
+    {
+        nombre: 'Pruebas Automatización',
+        porcentaje: '60%',
+        valor: '60',
+    },
+]
+
+function mostrarSkills(skill) {
+    let divSkill = document.createElement('div')
+    divSkill.classList.add('progress')
+    divSkill.innerHTML =
+        `<h4>${skill.nombre}</h4>
+            <div class="progress-bar">
+              <span data-width="${skill.porcentaje}">${skill.valor}<sup>%</sup></span>
+            </div>`
+    skills.append(divSkill)
+}
+
+habilidades.forEach(skill => {
+    mostrarSkills(skill)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const spans = document.querySelectorAll('.progress-bar span')
+    spans.forEach((span) => {
+        let spanInner = span.style.width = span.dataset.width
+        span.innerHTML = spanInner
+    })
 })
